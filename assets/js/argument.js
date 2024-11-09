59c721d8cf4212c819d20d45f700f73459566c8b
sumonrst <eng.sumonrejata@gmail.com>
2024-11-10 02:54:10 +0600

    all section color fix

---
+++ assets/js/argument.js
@@ -0,0 +1,18 @@
+
+// function myArgument(){
+//     if ( arguments.length == 0 ){
+//         console.log("No Argument Found");
+//     }else{
+//         let store = 0;
+//         for ( let i = 0; i < arguments.length; i++ ){
+//             store = store + arguments[i];
+//         }
+//         console.log(store);
+//     }
+// }
+// myArgument(2, 3);
+
+
+
+
+
